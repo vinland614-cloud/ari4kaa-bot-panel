76665f4e6779fb4c37192772bfcb521d82f64f9d
vinland614-cloud <vinland614@gmail.com>
2026-03-26 18:43:13 +0200

    Update script.js

--- web/script.js
+++ web/script.js
@@ -62,7 +62,6 @@ function clearList() {
   fetch('/api/reset', { method: 'POST' });
 }
 
-/* Галочки участников */
 function toggleUser(user) {
   fetch('/api/toggle', {
     method: 'POST',
@@ -71,39 +70,59 @@ function toggleUser(user) {
   });
 }
 
+/* Участники */
 socket.on('participants', data => {
   const list = document.getElementById('participantsList');
   list.innerHTML = '';
 
   data.participants.forEach(user => {
     const li = document.createElement('li');
+    li.className = 'participantItem';
 
+    // левая часть (иконка + ник)
+    const left = document.createElement('div');
+    left.className = 'leftPart';
+
+    const icon = document.createElement('span');
+    icon.innerText = '👤';
+
+    const name = document.createElement('span');
+    name.innerText = user;
+
+    left.appendChild(icon);
+    left.appendChild(name);
+
+    // правая часть (галочка)
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = data.participantData[user].active;
     checkbox.onchange = () => toggleUser(user);
 
+    li.appendChild(left);
     li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(' ' + user));
 
     if (!data.participantData[user].active) {
-      li.style.color = 'gray';
+      li.style.opacity = '0.4';
     }
 
     list.appendChild(li);
   });
 });
 
+/* Чат + сообщения победителя */
 socket.on('winnerMessages', data => {
   const chat = document.getElementById('chat');
+
   const div = document.createElement('div');
   div.innerText = data.user + ': ' + data.message;
+
   chat.appendChild(div);
   chat.scrollTop = chat.scrollHeight;
 
   if (data.user === currentWinner) {
     const msg = document.createElement('div');
     msg.innerText = data.message;
+
     document.getElementById('winnerMessages').prepend(msg);
 
     clearInterval(timerInterval);
